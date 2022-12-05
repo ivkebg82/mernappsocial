72df39a2affebb7d7764bfd530c6cf8baba35fbf
ivkebg82 <grcaivan@gmail.com>
2022-12-05 08:34:11 +0100

    added user model

---
+++ server/Models/UserModel.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        lowercase: true,
+    },
+    profileImg: String,
+    coverImg: String,
+    worksAt: String,
+    status: String,
+})
+
+
+const UserModel = mongoose.model('User', UserSchema)
+
+export default UserModel;
